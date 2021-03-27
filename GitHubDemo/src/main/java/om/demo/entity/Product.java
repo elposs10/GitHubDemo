5c3566cce4502e11cac8b1b3b716dc9865740c45
Oussema Mihoubi <oussema.mihoubi@esprit.tn>
2021-03-27 22:33:09 +0100

    Product Getters & Setters

--- GitHubDemo/src/main/java/om/demo/entity/Product.java
+++ GitHubDemo/src/main/java/om/demo/entity/Product.java
@@ -21,5 +21,89 @@ public class Product implements Serializable {
 	@Column(name = "Prod_PRICE")
 	private String Price;
 
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Product(Long id, String name, String price) {
+		super();
+		Id = id;
+		Name = name;
+		Price = price;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getPrice() {
+		return Price;
+	}
+
+	public void setPrice(String price) {
+		Price = price;
+	}
+
+	public static Long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((Name == null) ? 0 : Name.hashCode());
+		result = prime * result + ((Price == null) ? 0 : Price.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		if (Name == null) {
+			if (other.Name != null)
+				return false;
+		} else if (!Name.equals(other.Name))
+			return false;
+		if (Price == null) {
+			if (other.Price != null)
+				return false;
+		} else if (!Price.equals(other.Price))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [Id=" + Id + ", Name=" + Name + ", Price=" + Price + "]";
+	}
+	
+	
+
 	
 }
