8ca79c7304e5b421fba00581fcc6c286d197eff0
tarek-elakermi <Elakermi.Pro@gmail.com>
2021-03-27 17:21:33 -0400

    Update Product.java

--- GitHubDemo/src/main/java/om/demo/entity/Product.java
+++ GitHubDemo/src/main/java/om/demo/entity/Product.java
@@ -14,6 +14,7 @@ public class Product implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "Prod_ID")
 	private Long Id;
+	
 	@Column(name = "Prod_NAME")
 	private String Name;
 	
