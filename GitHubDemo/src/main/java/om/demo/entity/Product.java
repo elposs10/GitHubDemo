5ea640804b69c5c7caeb15f91e07e4d5055b34ca
tarek-elakermi <Elakermi.Pro@gmail.com>
2021-03-27 17:20:52 -0400

    Create Product.java
    Product Entity

---
+++ GitHubDemo/src/main/java/om/demo/entity/Product.java
@@ -0,0 +1,24 @@
+package om.demo.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="T_PRODUCT")
+public class Product implements Serializable {
+
+	private static final Long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Prod_ID")
+	private Long Id;
+	@Column(name = "Prod_NAME")
+	private String Name;
+	
+	@Column(name = "Prod_PRICE")
+	private String Price;
+
+	
+}
