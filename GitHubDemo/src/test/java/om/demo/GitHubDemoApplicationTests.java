b5053fbbaaa333a45175c681747f03b31613b855
Elposs10 <oussema.mihoubi@esprit.tn>
2021-03-27 17:02:28 +0100

    Commit all files

---
+++ GitHubDemo/src/test/java/om/demo/GitHubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package om.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
